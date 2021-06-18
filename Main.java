876e032a05924dd0baed929f26d6acfa30f35125
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-06-18 12:21:32 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package odd;
+
+public class odd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Product_Of_OddNo in=new Product_Of_OddNo();
+		in.Product_of_oddno();
+		in=null;	
+	}
+}
